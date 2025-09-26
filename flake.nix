233ce9f364d989e98d8cec85ec4568bb12bad9b6
Tim Hoffman <timothy.hoffman@veridise.com>
2025-09-25 23:59:24 -0500

    hide different version of llvm from the start

--- flake.nix
+++ flake.nix
@@ -29,6 +29,9 @@
     {
       packages = flake-utils.lib.flattenTree {
         inherit (pkgs) mlir mlir-debug;
+        # Prevent use of libllvm and llvm from nixpkgs, which will have 
+        # different versions than mlir/llvm built here.
+        inherit (pkgs.llzk-llvmPackages) libllvm llvm;
       };
 
       formatter = pkgs.nixpkgs-fmt;
